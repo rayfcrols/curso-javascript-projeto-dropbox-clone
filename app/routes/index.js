727bb766152bce8b633efb8eaac0573a5b5483a5
Raimundo Luiz <rayfcrols@gmail.com>
2019-09-27 13:08:53 -0300

    Rename e Deleto Concluido

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+
+const router = express.Router();
+
+const formidable = require('formidable');
+
+const fs = require('fs');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
+});
+
+/* POST home page. */
+router.post('/upload', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+  form.parse(req, (err, fields, files) => {
+    res.json({ files });
+  });
+});
+
+/* DELETE home page. */
+router.delete('/file', (req, res) => {
+  let form = new formidable.IncomingForm({
+    uploadDir: './upload',
+    keepExtensions: true
+  });
+  form.parse(req, (err, fields, files) => {
+    let path = './' + fields.path;
+    if (fs.existsSync(path)) {
+      fs.unlink(path, (err) => {
+        if (err) {
+          res.status(400).json({
+            err
+          });
+        } else {
+          res.json({ fields });
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
